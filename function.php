994e13fb40057fbc27f40c8ff4d7093af23c34e7
Akash <akashpise588@gmail.com>
2021-11-08 11:55:36 +0530

    screenshot_uploaded

--- function.php
+++ function.php
@@ -4,6 +4,6 @@ add_action( 'wp_enqueue_scripts', 'astra_child_enqueue_styles' );
 function astra_child_enqueue_styles() {
     wp_enqueue_style( 'astra-child-style', get_stylesheet_uri(),
         array( 'astra-theme-css' ), 
-        wp_get_theme()->get('1.0.0') // this only works if you have Version in the style header
+        wp_get_theme()->get('1.0.0')  
     );
 }
